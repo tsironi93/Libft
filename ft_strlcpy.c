b23a26a54f4e6816eae4aa4e111dc38be1bebfbd
Ioannis Tsiros <itsiros@2-G-4.42heilbronn.de>
2025-04-26 19:49:04 +0200

    asd

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: itsiros <itsiros@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 22:16:27 by itsiros           #+#    #+#             */
-/*   Updated: 2024/10/28 22:34:14 by itsiros          ###   ########.fr       */
+/*   Updated: 2025/03/29 12:56:25 by itsiros          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	srcsize;
-	int		i;
 
 	srcsize = ft_strlen(src);
-	i = 0;
 	if (dstsize > srcsize + 1)
 		ft_memcpy(dst, src, srcsize + 1);
 	else if (dstsize != 0)
